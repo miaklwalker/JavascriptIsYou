17a3ac9c7d7bc5f8a9f8f2ce0398ec60060ff21b
Michael Walker <ouisihai@gmail.com>
2019-05-12 10:25:25 -0500

    added rule set

--- src/GameFiles/Rules.js
+++ src/GameFiles/Rules.js
@@ -1,8 +1,11 @@
 import { objects } from "./blockDraw.js";
 import { GameInfo } from "./gameInfo.js";
+import { operatorBlocks } from "../Blocks/BlocksCache.js";
 
 export let Rules = {};
 
 export function setRules() {
-
+operatorBlocks.forEach(block=>{
+     Rules[block.ruleOne[0]] = [block.rule[1]];
+})
 }
